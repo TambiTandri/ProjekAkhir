162ea0df06184236c1d9ff6b235b0c236bf6d905
Fajar133 <100067524+fajar033@users.noreply.github.com>
2023-10-19 14:53:26 +0700

    update

--- app/src/main/java/com/example/projekakhir/HomeFragment.kt
+++ app/src/main/java/com/example/projekakhir/HomeFragment.kt
@@ -28,7 +28,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -37,30 +37,21 @@ class HomeFragment : Fragment() {
         binding.menu.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_menuFragment)
         }
-        binding.cvFcIjazahSma.setOnClickListener {
-            handlerCardViewClikc(binding.cvFcIjazahSma)
+        binding.btnDaftar.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
         }
-        binding.cvFcAkteLahir.setOnClickListener {  }
-        binding.cvFcKK.setOnClickListener {  }
-        binding.cvFcKTP.setOnClickListener {  }
-        binding.cvFcKTM.setOnClickListener {  }
-        binding.cvPasFoto.setOnClickListener {  }
-        binding.cvSkl.setOnClickListener {  }
-        binding.cvSertifToeic.setOnClickListener {  }
-        resetCardViewColors()
+        binding.cvFcIjazahSma.setOnClickListener { selectedIndex(0) }
+        binding.cvFcAkteLahir.setOnClickListener { selectedIndex(1) }
+        binding.cvFcKK.setOnClickListener { selectedIndex(2) }
+        binding.cvFcKTP.setOnClickListener { selectedIndex(3) }
+        binding.cvFcKTM.setOnClickListener { selectedIndex(4) }
+        binding.cvPasFoto.setOnClickListener { selectedIndex(5) }
+        binding.cvSkl.setOnClickListener { selectedIndex(6) }
+        binding.cvSertifToeic.setOnClickListener { selectedIndex(7) }
     }
 
-    private fun handlerCardViewClikc(cardView:CardView){
-        resetCardViewColors()
-
-        val linearLayout =cardView.findViewById<LinearLayout>(R.id.linearLayout)
-        val ivCheck = cardView.findViewById<ShapeableImageView>(R.id.ivCheck1)
-        linearLayout.setBackgroundColor(resources.getColor(R.color.biru))
-        ivCheck.setColorFilter(resources.getColor(R.color.biru))
-    }
-
-    private fun resetCardViewColors() {
-        val cardViews = listOf(
+    private fun selectedIndex(selectedIndex: Int) {
+        val selectCvList = listOf(
             binding.cvFcIjazahSma,
             binding.cvFcAkteLahir,
             binding.cvFcKK,
@@ -70,67 +61,46 @@ class HomeFragment : Fragment() {
             binding.cvSkl,
             binding.cvSertifToeic
         )
-        for(cardView in cardViews){
-            val linearLayout = cardView.findViewById<LinearLayout>(R.id.linearLayout)
-            val ivCheck = cardView.findViewById<ShapeableImageView>(R.id.ivCheck1)
-            linearLayout.setBackgroundColor(resources.getColor(R.color.white))
-            ivCheck.setColorFilter(resources.getColor(R.color.black))
+        selectCvList.forEachIndexed { index, selectCv ->
+            val isSelected = (index == selectedIndex)
+            val iconColorRes = if (isSelected) R.color.white else R.color.black
+            val linearColorRes = if (isSelected) R.color.biru else R.color.white
+
+            selectCv.findViewById<ShapeableImageView>(getIconColor(index))
+                .setColorFilter(resources.getColor(iconColorRes))
+            selectCv.findViewById<LinearLayout>(getLinearColor(index))
+                .setBackgroundColor(resources.getColor(linearColorRes))
         }
     }
 
-//    private fun selectedIndex(selectIndex:Int){
-//        val cvSelectList = listOf(
-//            binding.cvFcIjazahSma,
-//            binding.cvFcAkteLahir,
-//            binding.cvFcKK,
-//            binding.cvFcKTP,
-//            binding.cvFcKTM,
-//            binding.cvPasFoto,
-//            binding.cvSkl,
-//            binding.cvSertifToeic
-//        )
-//        cvSelectList.forEachIndexed { index, cvList ->
-//            val isSelected = (index == selectIndex)
-//            val iconColor = if(isSelected){
-//                R.color.white
-//            }else{
-//                R.color.black
-//            }
-//            val backgroundColorRes = if (isSelected){
-//                R.color.biru
-//            }else{
-//                R.color.white
-//            }
-//            cvList.findFragment<LinearLayout>(layoutIndex(index)).setBackgroundColor(resources.getColor(backgroundColorRes))
-//            cvList.findViewById<ShapeableImageView>(checklisIcon(index)).setBackgroundColor(resources.getColor(iconColor))
-//        }
-//    }
-//
-//    private fun checklisIcon(index : Int):Int{
-//        return when(index){
-//            0 -> R.id.ivCheck1
-//            1 -> R.id.ivCheck2
-//            2 -> R.id.ivCheck3
-//            3 -> R.id.ivCheck4
-//            4 -> R.id.ivCheck5
-//            5 -> R.id.ivCheck6
-//            6 -> R.id.ivCheck7
-//            7 -> R.id.ivCheck8
-//            else -> throw IllegalArgumentException("Invalid index")
-//        }
-//    }
-//
-//    private fun layoutIndex(index:Int):Int{
-//        return when(index){
-//            0 -> R.id.linearLayout
-//            1 -> R.id.linearLayout2
-//            2 -> R.id.linearLayout3
-//            3 -> R.id.linearLayout4
-//            4 -> R.id.linearLayout5
-//            5 -> R.id.linearLayout6
-//            6 -> R.id.linearLayout7
-//            7 -> R.id.linearLayout8
-//            else -> throw IllegalArgumentException("Invalid Index")
-//        }
-//    }
+    private fun getIconColor(index: Int): Int {
+        return when (index) {
+            0 -> R.id.ivCheck1
+            1 -> R.id.ivCheck2
+            2 -> R.id.ivCheck3
+            3 -> R.id.ivCheck4
+            4 -> R.id.ivCheck5
+            5 -> R.id.ivCheck6
+            6 -> R.id.ivCheck7
+            7 -> R.id.ivCheck8
+            else -> throw IllegalArgumentException("Invalid index")
+        }
+    }
+
+    private fun getLinearColor(index: Int): Int {
+        return when (index) {
+            0 -> R.id.linearLayout
+            1 -> R.id.linearLayout2
+            2 -> R.id.linearLayout3
+            3 -> R.id.linearLayout4
+            4 -> R.id.linearLayout5
+            5 -> R.id.linearLayout6
+            6 -> R.id.linearLayout7
+            7 -> R.id.linearLayout8
+            else -> throw IllegalArgumentException("Invalid index")
+        }
+
+
+
+    }
 }
