f2e946944430c3b5f7a08d503b68661c8904cc12
TambiTandri <yamantandri@gmail.com>
2023-10-24 15:16:29 +0700

    update

--- app/src/main/java/com/example/projekakhir/DashboardFragment.kt
+++ app/src/main/java/com/example/projekakhir/DashboardFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.projekakhir.databinding.FragmentDashboardBinding
 
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var binding: FragmentDashboardBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.logout.setOnClickListener{
+            findNavController().navigate(R.id.action_dashboardFragment_to_homeFragment)
+        }
+
+
+    }
+
 
 }
