e0e646eccbc614074d29e9a6cd0b8e97b1b1cf68
TambiTandri <yamantandri@gmail.com>
2023-11-23 10:42:43 +0700

    update

--- app/src/main/java/com/example/projekakhir/BiodataPendaftaranFragment.kt
+++ app/src/main/java/com/example/projekakhir/BiodataPendaftaranFragment.kt
@@ -111,7 +111,188 @@ class BiodataPendaftaranFragment : Fragment() {
         binding.inputpembimbingagama.setOnClickListener {
             pickPembimbingAgama()
         }
-        binding.uploadButton1.setOnClickListener { showFileIjazah()}
+        binding.uploadButton1.setOnClickListener {
+            showFileIjazah()}
+        binding.uploadButton2.setOnClickListener {
+            showFileAktekelahiran()
+        }
+        binding.uploadButton3.setOnClickListener {
+            showFilefckk()
+        }
+        binding.uploadButton4.setOnClickListener {
+            showFileFcKtp()
+        }
+        binding.uploadButton5.setOnClickListener {
+            showFileFcKtm()
+        }
+        binding.uploadButton6.setOnClickListener {
+            showFilePasFoto()
+        }
+        binding.uploadButton7.setOnClickListener {
+            showFileSkl()
+        }
+        binding.uploadButton8.setOnClickListener {
+            showFileSt()
+        }
+    }
+
+    private fun showFileSt() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentSt.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentSt = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+        }
+    }
+
+    private fun showFileSkl() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentSkl.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentSkl = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton7.visibility = View.GONE
+            binding.tvSkl.text = it.data?.data.toString()
+        }
+    }
+
+    private fun showFilePasFoto() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentPasFoto.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentPasFoto = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton6.visibility = View.GONE
+            binding.tvPasFoto.text = it.data?.data.toString()
+        }
+    }
+
+    private fun showFileFcKtm() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentFcKtm.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentFcKtm = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton5.visibility = View.GONE
+            binding.tvFcKtm.text = it.data?.data.toString()
+        }
+    }
+
+    private fun showFileFcKtp() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentFcKtp.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentFcKtp = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton4.visibility = View.GONE
+            binding.tvFcKtp.text = it.data?.data.toString()
+        }
+    }
+
+    private fun showFilefckk() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentfckk.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentfckk = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton3.visibility = View.GONE
+            binding.tvFcKk.text = it.data?.data.toString()
+        }
+    }
+
+    private fun showFileAktekelahiran() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentAkte.launch(intent)
+//            startActivityForResult(Intent.createChooser(intent,"Select a File"),
+//                100)
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentAkte = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK){
+            binding.uploadButton2.visibility = View.GONE
+            binding.tvFcAkte.text = it.data?.data.toString()
+        }
     }
 
     private fun showFileIjazah() {
--- app/src/main/java/com/example/projekakhir/SklFragment.kt
+++ app/src/main/java/com/example/projekakhir/SklFragment.kt
@@ -1,12 +1,17 @@
 package com.example.projekakhir
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.projekakhir.databinding.FragmentSklBinding
+import java.lang.Exception
 
 
 class SklFragment : Fragment() {
@@ -32,5 +37,81 @@ class SklFragment : Fragment() {
         binding.logoutskl.setOnClickListener{
             findNavController().navigate(R.id.action_sklFragment_to_loginFragment)
         }
+        binding.buttonartikel.setOnClickListener {
+           pickFileArtikel()
+        }
+        binding.btnlp.setOnClickListener {
+            pickFilelp()
+        }
+        binding.btnsk.setOnClickListener {
+            pickFilesk()
+        }
     }
+
+    private fun pickFileArtikel() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentArtikel.launch(intent)
+
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentArtikel = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode== Activity.RESULT_OK){
+            binding.fileArtikel.text = it.data?.data.toString()
+
+        }
+    }
+
+    private fun pickFilelp() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentlp.launch(intent)
+
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentlp = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode== Activity.RESULT_OK){
+            binding.filelp.text = it.data?.data.toString()
+
+        }
+    }
+
+    private fun pickFilesk() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            launcherIntentsk.launch(intent)
+
+        }catch (exception:Exception){
+            Toast.makeText(requireActivity(),"Please install a file manager.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val launcherIntentsk = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode== Activity.RESULT_OK){
+            binding.filesk.text = it.data?.data.toString()
+
+        }
+    }
+
 }
